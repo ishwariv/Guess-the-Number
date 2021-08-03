8cbf3cfbe250b16dfebdcebd4a0cf037f14f9df6
Ishwari Veerkar <70537425+ishwariv@users.noreply.github.com>
2021-08-02 21:50:50 -0400

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,11 +1,43 @@
-number = 4
-chance = 0
-limit = 3
-while chance<limit:
-    guess = int(input('Guess: '))
-    chance=chance+1
-    if guess == number:
-        print("You win!")
-        break
-else:
-    print("Sorry, you failed!")
+import random
+
+#number of games
+games_number=1
+max_games=5
+
+#number of chances in a game
+chances=5
+
+#difficulty level
+difficulty = 0
+
+score= []
+
+#input for difficulty level
+while difficulty>3 or difficulty<1:
+  print("Difficulty Level 1-3: ")
+  difficulty = int(input())
+
+max_number = difficulty*25
+
+#game 1
+while games_number <= max_games:
+  print("Game " + str(games_number))
+  randomNumber = random.randint(0,max_number)
+  currentChance = 1
+  while currentChance<chances:
+    print("Guess a Number Between 0 and " + str(max_number) + ": ")
+    guess = int(input())
+    if(guess == randomNumber):
+     print("You Guessed Correctly")
+     score.append("W")
+     break;
+    elif(guess < randomNumber):
+     print("You Guessed Lower Than the Number")
+    elif(guess > randomNumber):
+     print("You Guessed Higher Than the Number")
+    if(currentChance == 5):
+     print("You Lost! The number was " + str(randomNumber))  
+  score.append("L")
+currentChance += 1
+games_number += 1
+print(*score)
